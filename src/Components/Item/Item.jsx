61817ee25e8af5a2cd479a79611471c010ed87ed
ShivamGupta322 <guptashivamsg02@gmail.com>
2024-08-03 12:26:21 +0530

    poplura items

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 function Item(props) {
   return (
-    <div className=' w-[350px]'>
+    <div className=' w-[350px] hover:scale-105'>
         <img src={props.image} alt="" />
         <p className=''>{props.name}</p>
         <div className="flex gap-[20px]">
             <div className="text-[#374151] text-sm font-semibold ">
-               {props.new_price} 
+            ₹{props.new_price} 
             </div>
-            <div className="item-price-old">
-                {props.old_price} 
+            <div className="text-[#8c8c8c] text-sm font-semibold hover:scale-105 hover:-translate-y-1">
+            ₹{props.old_price} 
             </div>
         </div>
     </div>
--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
+import Popular from '../Components/Popular/Popular'
+import Offers from '../Components/Offers/Offers'
 
 const Shop = () => {
   return (
-    <div><Hero/></div>
+    <div>
+      <Hero/>
+      <Popular/>
+      <Offers />
+    </div>
   )
 }
 
