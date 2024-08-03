53031f350a0a301dbc0dc2d58ddffcf0f378f36c
RobinSinghYadav <githubrobin08@gmail.com>
2024-08-03 11:03:38 +0530

    Third Commit

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Hero from '../Components/Hero/Hero'
+import Popular from '../Components/Popular/Popular'
+import Offers from '../Components/Offers/Offers'
+import NewCollections from '../Components/NewCollections/NewCollections'
 
 const Shop = () => {
   return (
-    <div>Shop</div>
+    <div>
+      <Hero />
+      <Popular />
+      <Offers />
+      <NewCollections/>
+      <NewsLetter/>
+
+    </div>
   )
 }
 
