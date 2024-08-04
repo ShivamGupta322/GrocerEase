a3bbd3e0d3274da1ddcd6669da022975269e1cbc
RobinSinghYadav <githubrobin08@gmail.com>
2024-08-04 15:36:31 +0530

    4july commit

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Item(props) {
   return (
-    <div className=' w-[350px] hover:scale-105'>
-        <img className='h-[30vh]' src={props.image} alt="" />
+    <div className=' w-[350px] hover:scale-105 item'>
+        <Link to={`/product/${props.id}`}>
+          <img className='h-[30vh]' src={props.image} alt="" />
+        </Link>
         <p className=''>{props.name}</p>
-        <div className="flex gap-[20px]">
-            <div className="text-[#374151] text-sm font-semibold ">
+        <div className="flex gap-[20px] item-prices">
+            <div className="text-[#374151] text-sm font-semibold item-prices-new">
             ₹{props.new_price} 
             </div>
-            <div className="text-[#8c8c8c] text-sm font-semibold hover:scale-105 hover:-translate-y-1">
+            <div className="text-[#8c8c8c] text-sm font-semibold hover:scale-105 hover:-translate-y-1 item-prices-old">
             ₹{props.old_price} 
             </div>
         </div>
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,12 +1,19 @@
 import React, { useContext } from 'react'
 import { ShopContext } from '../Components/Context/ShopContext'
+import {useParams} from 'react-router-dom';
+import Breadcrum from '../Components/Breadcrums/Breadcrum'
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay'
+
 
 const Product = () => {
-  const [all_product]=useContext(ShopContext)
-
-  
+  const {all_product}=useContext(ShopContext);
+  const {productId}=useParams();
+  const product=all_product.find((e)=>e.id===Number(productId));
   return (
-    <div>Product</div>
+    <div>
+      <Breadcrum product ={product}/>
+      <ProductDisplay product={product}/>
+    </div>
   )
 }
 
